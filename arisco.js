938bb75c788c0aed09803a56f31f37a6518e4d72
jaffarc <jaffar.cardoso@gmail.com>
2017-10-22 17:19:27 -0200

    add package and ajust js

--- arisco.js
+++ arisco.js
@@ -1,87 +1,83 @@
-const TelegramBot = require('node-telegram-bot-api');
-const json = require('./config.json');
-var exec = require('child_process').exec;
-var execSpawn = require('child_process').spawn;
+const TelegramBot = require('node-telegram-bot-api'),
+	json = require('./config.json'),
+	exec = require('child_process').exec,
+	execSpawn = require('child_process').spawn;
 
 
-const arisco = new TelegramBot(json.authorizationToken, { polling: true });
-const INDEX_COMMAND = 1;
+const arisco = new TelegramBot(json.authorizationToken, { polling: true }),
+	INDEX_COMMAND = 1;
 
-function execute(command, callback) {
-    exec(command, function(error, stdout, stderr){ callback(error, stdout, stderr); });
-}
+const getCustomCommand = () => {
+	for (var i = 0; i < json.config.customCommands.length; i++) {
+		if (json.config.customCommands[i][command] !== undefined) {
+			return json.config.customCommands[i];
+		}
+	}
+	return null;
+};
 
-function executeSpawn(command) {
-	var args = [];
-	if(command.length > 1)
+
+const execute = (command, callback) => exec(command, (error, stdout, stderr) => { callback(error, stdout, stderr); });
+
+
+const executeSpawn = (command) => {
+	let args = [];
+	if (command.length > 1) {
 		args = command.slice(1);
+	}
 
 	execSpawn(command[0], args, { detached: true, shell: true })
 		.on('error', (err) => console.log(err));
 }
 
 arisco.onText(/\/raspberry (.+)/, (msg, match) => {
-	const chatId = msg.chat.id;
-	const command = match[INDEX_COMMAND];
+	let chatId = msg.chat.id,
+		command = match[INDEX_COMMAND];
 
-	var getCustomCommand = () => {
-		for(var i = 0; i < json.config.customCommands.length; i++) {
-			if(json.config.customCommands[i][command] !== undefined) {
-				return json.config.customCommands[i];
-			}
-		}
-		return null;
-	}
 
 	var customCommand = getCustomCommand();
 
-	if(customCommand !== null) {
-		if(!json.config.adminUsers.includes(chatId)) { 
+	if (customCommand !== null) {
+		if (!json.config.adminUsers.includes(chatId)) {
 			arisco.sendMessage(chatId, 'You are not allowed to perform this command!');
 			return;
 		}
-		
+
 		console.log('Executing custom command: ', customCommand);
 
-		if(customCommand.executeWithSpawn) {
+		if (customCommand.executeWithSpawn) {
 			console.log("executing: ", customCommand[command]);
-			executeSpawn(customCommand[command]);
+			return executeSpawn(customCommand[command]);
 		}
-		else
-		{
-			execute(customCommand[command], function(error, stdout, stderr){
-				arisco.sendMessage(chatId, '<code>' + stdout + stderr + '</code>', { parse_mode: 'HTML' });
-			});
+
+		return execute(customCommand[command], (error, stdout, stderr) => arisco.sendMessage(chatId, '<code>' + stdout + stderr + '</code>', { parse_mode: 'HTML' }));
+
+	};
+
+	existDeniedCommands = (command) => {
+		for (var i = 0; i < json.config.deniedCommands.length; i++) {
+			if (command.indexOf(json.config.deniedCommands[i]) > -1)
+				return true;
 		}
+		return false;
+	};
+
+	if (existDeniedCommands(command) &&
+		!json.config.adminUsers.includes(chatId)) {
+		arisco.sendMessage(chatId, 'You are not allowed to perform this command!');
+		return;
 	}
-	else {
 
-		existDeniedCommands = (command) => {
-			for(var i = 0; i < json.config.deniedCommands.length; i++){
-				if(command.indexOf(json.config.deniedCommands[i]) > -1)
-					return true;
-			}
-			return false;
-		};
+	execute(command, (error, stdout, stderr) => arisco.sendMessage(chatId, '<code>' + stdout + stderr + '</code>', { parse_mode: 'HTML' }));
 
-		if(existDeniedCommands(command) && 
-			!json.config.adminUsers.includes(chatId)) {
-			arisco.sendMessage(chatId, 'You are not allowed to perform this command!');
-			return;
-		}
 
-		execute(command, function (error, stdout, stderr){
-			arisco.sendMessage(chatId, '<code>' + stdout + stderr + '</code>', { parse_mode: 'HTML' });
-		});
-
-	}
 });
 
 arisco.onText(/\/selfie/, (msg, match) => {
 	const chatId = msg.chat.id;
 	var index = Math.round(Math.random() * (json.config.selfies.length - 0) + 0);
 
-	if( index < json.config.selfies.length)
+	if (index < json.config.selfies.length)
 		arisco.sendPhoto(chatId, json.config.selfies[index]);
 });
 
