e37f01407e5d97ac39139cb83985ace92c44d3c4
Thiago <tsigwt@gmail.com>
2017-10-26 21:10:49 -0200

    Removing the ;

--- arisco.js
+++ arisco.js
@@ -60,7 +60,7 @@ arisco.onText(/\/raspberry (.+)/, (msg, match) => {
 				return true;
 		}
 		return false;
-	};
+	}
 
 	if (existDeniedCommands(command) &&
 		!json.config.adminUsers.includes(chatId)) {
