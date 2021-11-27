2c4df3af7383347578a3244dd85dca7086abe21c
Thiago Sanches <tsigwt@gmail.com>
2021-11-27 11:14:02 -0300

    Update arisco.js

--- arisco.js
+++ arisco.js
@@ -139,7 +139,7 @@ arisco.onText(/\/iot (.+)/gm, async (msg, match) => {
         }
         else if (command === 'p') {
             console.log("Taking a picture...")
-            const picture = await axios.get(`${json.config.esp32cam}/cam-hi.jpg`, {
+            const picture = await axios.get(`${json.config.esp32cam}/jpg`, {
                 responseType: 'stream'
             })
             await picture.data.pipe(fs.createWriteStream("./picture.jpg"));
