6c5cc86fbc285903bbcc3c700344647dc21bc071
Thiago Sanches <tsigwt@gmail.com>
2022-04-23 09:23:34 -0300

    Rename the file

---
+++ main.js
@@ -0,0 +1,29 @@
+const TelegramBot = require('node-telegram-bot-api');
+const json = require('./config.json');
+const selfie = require('./commands/fun/selfie');
+const message = require('./commands/default/message');
+const getWhatsAppDirectLink = require('./commands/utils/getWhatsAppDirectLink');
+const getOnlyNumbersFromText = require('./commands/utils/getOnlyNumbersFromText');
+const p = require('./commands/iot/p');
+const c = require('./commands/iot/c');
+const m = require('./commands/iot/m');
+const r = require('./commands/raspberry/r');
+const bot = new TelegramBot(json.authorizationToken, { polling: true });
+
+/*DEFAULT*/
+bot.on('message', (msg) => { message.execute(msg, json, bot) });
+
+/*RASPBERRY*/
+bot.onText(/\/r (.+)/, (msg, match) => { r.execute(json, msg, match, bot) });
+
+/*FUN*/
+bot.onText(/\/selfie/, (msg) => { selfie.execute(json, msg.chat.id, bot) });
+
+/*UTILS*/
+bot.onText(/\/t (.+)/gm, (msg) => { getOnlyNumbersFromText.execute(msg, msg.chat.id, bot) });
+bot.onText(/\/w (.+)/gm, (msg, match) => { getWhatsAppDirectLink.execute(match, msg.chat.id, bot) });
+
+/*IOT*/
+bot.onText(/\/iot p/, async (msg) => { p.execute(json, msg.chat.id, bot) });
+bot.onText(/\/iot c=(.+)/, async (msg, match) => { c.execute(json, msg, match, bot) });
+bot.onText(/\/iot m=(.+)/, async (msg, match) => { m.execute(json, msg, match, bot) });
