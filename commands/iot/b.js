4677b033de904ee08271558e3a580c9511330f39
Thiago Sanches <tsigwt@gmail.com>
2023-07-09 16:55:09 -0300

    Add some timeout to make it appear

--- commands/iot/b.js
+++ commands/iot/b.js
@@ -3,11 +3,18 @@ const axios = require('axios');
 module.exports.execute = async function (json, msg, match, bot) {
     console.log('Executing b.js');
     const chatId = msg.chat.id;
+    
     await axios.post(`${json.config.bulb}/json`, { on: false });
+    await new Promise(resolve => setTimeout(resolve, 500));
+
     await axios.post(`${json.config.bulb}/json`, {
         seg: [{ col: [match[1].split(',')], fx: 2, sx: 255, ix: 255 }]
     });
+    await new Promise(resolve => setTimeout(resolve, 500));
+
     await axios.post(`${json.config.bulb}/json`, { on: true, bri: 255 });
+    await new Promise(resolve => setTimeout(resolve, 500));
+
     await axios.post(`${json.config.bulb}/json`, { on: false });
     bot.sendMessage(chatId, "💡 Bulb blinking!");
 };
